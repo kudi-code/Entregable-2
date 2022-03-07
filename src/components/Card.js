3c1774f011df721f1d5cb84dba1beec5e6a733e0
Kevin Salvador Casas <kevinsal.casas@gmail.com>
2022-03-06 22:20:56 -0600

    First Commit

---
+++ src/components/Card.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+const Card = ({country,weatherMain,speed,cloud,cloudImg,pressure,temp}) =>{
+    const[isFarenheit,setFarenheit] = useState([true, "°F", 0]) 
+    const[icon,setIcon] = useState("") 
+
+    useEffect(() => {
+        convertDegrees()
+        setIcon(cloudImg)
+        // return convertDegrees
+    } , [temp])
+    const convertDegrees = () =>{
+        if(isFarenheit[0] ===true){            
+            setFarenheit([false,"°C", Math.round(temp-273.15) ])
+        }
+        else{
+            setFarenheit([true,"°F", Math.round((temp-237.15)*1.8+32)])
+        }
+    }
+    console.log(isFarenheit[2])
+
+    return(
+        <div className="weather">
+            <h2>The Weather App</h2>
+            <h4>{country}</h4>
+            <img src={icon} />
+            <p>{weatherMain}</p>
+            <p><b>Wind Speed:</b> {speed} m/s</p>
+            <p><b>Clouds: </b>{cloud} %</p>
+            <p><b>Pressure: </b>{pressure} mb</p>
+            <p><b>{isFarenheit[2] === NaN ? "0" : isFarenheit[2] } {isFarenheit[1]}</b></p>
+        <button onClick={convertDegrees}>Degrees °F/°C</button>
+
+        </div>
+    );
+}
+export default Card;
